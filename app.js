98854a1b2ea86f4f3ec7ac1052e5cf8b162efc2e
Tyler Parker <tyler.m.parker14@gmail.com>
2023-05-29 17:36:56 -0400

    First Upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click',()=>{
+    //Toggle Nav
+        nav.classList.toggle('nav-active');
+    //Animated Links
+    navLinks.forEach((link, index)=>{
+        if(link.style.animation){
+            link.style.animation = '';
+        }
+        else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index /9 + 0.5}s`;
+        }
+    });
+    //Button anim
+    burger.classList.toggle('toggle');
+    });
+    
+}
+
+navSlide();
+
